cc5446970fe0d04da36c9f4f533ff0e4781e8350
bartosz-walasek <b.wal1990@gmail.com>
2023-10-29 16:49:29 +0100

    verify adding product to the cart

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -15,11 +15,18 @@ describe("template spec", () => {
     cy.get(".block_6 > .nav > .dropdown > .dropdown-toggle > .caret").click();
     cy.contains("€ Euro").click();
     cy.url().should("include", "currency=EUR");
+    cy.get('[href="https://automationteststore.com/index.php?rt=product/category&path=58"]').click();
+    cy.get("ul.thumbnails").contains("Skincare").click();
+    cy.get(".contentpanel").contains("Men+Care Clean Comfort Deodorant").click();
+    cy.get("#product_quantity").clear().type("1");
+    cy.get(".cart").click();
+    cy.url().should("include", "checkout/cart");
+    cy.get('.product-list').should("contain.text", "Men+Care Clean Comfort Deodorant")
   });
 
-  afterEach("Logout from the App", () => {
-    cy.get("#customernav").click();
-    cy.get(".sidewidt").find('[href*="account/logout"]').click();
-    cy.get(".maintext").should("contain.text", " Account Logout");
-  });
+  // afterEach("Logout from the App", () => {
+  //   cy.get("#customernav").click();
+  //   cy.get(".sidewidt").find('[href*="account/logout"]').click();
+  //   cy.get(".maintext").should("contain.text", " Account Logout");
+  // });
 });

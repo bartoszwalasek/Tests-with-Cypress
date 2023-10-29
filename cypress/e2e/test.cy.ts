ea12a407dd272d02aadca5abfb741125bf4f2c5c
bartosz-walasek <b.wal1990@gmail.com>
2023-10-29 16:28:09 +0100

    Add beforeEach and afterEach to the test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,5 @@
 describe("template spec", () => {
-  it("passes", () => {
+  beforeEach("Login to the App", () => {
     cy.visit("/");
     cy.title().should("eq", "A place to practice your automation skills!");
     cy.contains("Login or register").click();
@@ -9,10 +9,17 @@ describe("template spec", () => {
     cy.get('[name="password"]').type("test_pass_wsb");
     cy.get('[title="Login"]').click();
     cy.url().should("include", "account/account");
-    cy.get('.block_6 > .nav > .dropdown > .dropdown-toggle > .caret').click()
+  });
+
+  it("passes", () => {
+    cy.get(".block_6 > .nav > .dropdown > .dropdown-toggle > .caret").click();
     cy.contains("€ Euro").click();
     cy.url().should("include", "currency=EUR");
+  });
+
+  afterEach("Logout from the App", () => {
+    cy.get("#customernav").click();
     cy.get(".sidewidt").find('[href*="account/logout"]').click();
-    cy.get('.maintext').should("contain.text", " Account Logout")
+    cy.get(".maintext").should("contain.text", " Account Logout");
   });
 });

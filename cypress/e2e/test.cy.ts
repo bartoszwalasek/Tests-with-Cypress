332c856cf6b3ec7fde37ffaebda9d84d64753f9e
Aszton <bartoszkuczera88@gmail.com>
2023-10-29 20:59:10 +0100

    added custom commands

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,26 +2,29 @@ describe("template spec", () => {
   beforeEach("Login to the App", () => {
     cy.visit("/");
     cy.title().should("eq", "A place to practice your automation skills!");
-    cy.contains("Login or register").click();
-    cy.get('[name="loginname"]')
-      .type("test_user_wsb")
-      .should("have.value", "test_user_wsb");
+    cy.findByTextAndClick("Login or register")
+    cy.findTypeAndCheckValue('[name="loginname"]', "test_user_wsb", "test_user_wsb")
     cy.get('[name="password"]').type("test_pass_wsb");
-    cy.get('[title="Login"]').click();
+    cy.findSelectorAndClick('[title="Login"]')
     cy.url().should("include", "account/account");
   });
 
   it("passes", () => {
-    cy.get(".block_6 > .nav > .dropdown > .dropdown-toggle > .caret").click();
+    cy.get(".dropdown-toggle").contains(" US Dollar").click();
     cy.contains("€ Euro").click();
     cy.url().should("include", "currency=EUR");
-    cy.get('[href="https://automationteststore.com/index.php?rt=product/category&path=58"]').click();
+    cy.findSelectorAndClick('[href="https://automationteststore.com/index.php?rt=product/category&path=58"]')
     cy.get("ul.thumbnails").contains("Skincare").click();
-    cy.get(".contentpanel").contains("Men+Care Clean Comfort Deodorant").click();
-    cy.get("#product_quantity").clear().type("1");
+    cy.get(".contentpanel")
+      .contains("Men+Care Clean Comfort Deodorant")
+      .click();
+    cy.clearInputAndType("#product_quantity", "1")
     cy.get(".cart").click();
     cy.url().should("include", "checkout/cart");
-    cy.get('.product-list').should("contain.text", "Men+Care Clean Comfort Deodorant")
+    cy.get(".product-list").should(
+      "contain.text",
+      "Men+Care Clean Comfort Deodorant"
+    );
   });
 
   // afterEach("Logout from the App", () => {
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,31 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+export {};
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      findTypeAndCheckValue(
+        selector: string,
+        text: string,
+        value: string
+      ): Chainable<void>;
+      findByTextAndClick(text: string): Chainable<void>;
+      findSelectorAndClick(selector: string): Chainable<void>;
+      clearInputAndType(selector: string, value: string): Chainable<void>;
+    }
+  }
+}
+
+Cypress.Commands.add("findTypeAndCheckValue", (selector, text, value) => {
+  cy.get(selector).type(text).should("have.value", value);
+});
+
+Cypress.Commands.add("findByTextAndClick", (text) => {
+  cy.contains(text).click();
+});
+
+Cypress.Commands.add("findSelectorAndClick", (selector) => {
+  cy.get(selector).click();
+});
+
+Cypress.Commands.add("clearInputAndType", (selector, value) => {
+  cy.get(selector).clear().type(value);
+});

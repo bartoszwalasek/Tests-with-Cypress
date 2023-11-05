db1f2be167fd7f1252bb88b724e785ff059f77ff
Aszton <bartoszkuczera88@gmail.com>
2023-11-05 20:58:37 +0100

    added selectors

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,7 +10,11 @@ declare global {
       findByTextAndClick(text: string): Chainable<void>;
       findSelectorAndClick(selector: string): Chainable<void>;
       clearInputAndType(selector: string, value: string): Chainable<void>;
-      assertUrl(assert: string, value: string): Chainable<void>;
+      assertUrl(value: string): Chainable<void>;
+      openUrlAndCheckTitle(page: string, title: string): Chainable<void>;
+      findSelectorTextAndClick(selector: string, text: string): Chainable<void>;
+      findElementAndCheckText(selector: string, text: string): Chainable<void>;
+      getSelectorFindSelectorAndClick(selector: string, find: string);
     }
   }
 }
@@ -27,10 +31,27 @@ Cypress.Commands.add("findSelectorAndClick", (selector) => {
   cy.get(selector).click();
 });
 
+Cypress.Commands.add("findSelectorTextAndClick", (selector, text) => {
+  cy.get(selector).contains(text).click();
+});
+
 Cypress.Commands.add("clearInputAndType", (selector, value) => {
   cy.get(selector).clear().type(value);
 });
 
-Cypress.Commands.add("assertUrl", (assert, value) => {
-  cy.url().should(assert, value);
+Cypress.Commands.add("assertUrl", (value) => {
+  cy.url().should("include", value);
+});
+
+Cypress.Commands.add("openUrlAndCheckTitle", (page, title) => {
+  cy.visit(page);
+  cy.title().should("eq", title);
+});
+
+Cypress.Commands.add("findElementAndCheckText", (selector, text) => {
+  cy.get(selector).should("contain.text", text);
+});
+
+Cypress.Commands.add("getSelectorFindSelectorAndClick", (selector, find) => {
+  cy.get(selector).find(find).click();
 });
